90d5dc756d610965da23ebd8f68a8ae2592b4972
LuisAngel <a20192832@pucp.edu.pe>
2023-10-21 09:45:00 -0500

    inicial

---
+++ Lab9_20192832/src/main/java/com/example/lab9_20192832/Lab920192832Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20192832;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920192832Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920192832Application.class, args);
+    }
+
+}
---
+++ Lab9_20192832/src/test/java/com/example/lab9_20192832/Lab920192832ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20192832;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920192832ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
